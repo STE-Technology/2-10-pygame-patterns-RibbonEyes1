13994d6c766837a191ad3f58f07125302f6010dc
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-03-22 10:59:37 -0400

    NOT DONE BUT GETTING THERE

--- patterns.py
+++ patterns.py
@@ -21,6 +21,7 @@ pygame.display.set_caption("Pygame Patterns")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN=(27, 135, 85)
+RED=(255,3,66)
 PURPLE=(116, 93, 156)
 # Main loop
 running = True
@@ -34,25 +35,21 @@ while running:
     # Draw graphics
     # --(start here)---------------------------------------------------------
     screen.fill(WHITE)
-    pygame.draw.circle(screen,GREEN,(730,90),20)
-    pygame.draw.circle(screen,GREEN,(650,90),20)
-    pygame.draw.circle(screen,GREEN,(570,90),20)
-    pygame.draw.circle(screen,GREEN,(490,90),20)
-    pygame.draw.circle(screen,GREEN,(730,170),20)
-    pygame.draw.circle(screen,GREEN,(650,170),20)
-    pygame.draw.circle(screen,GREEN,(570,170),20)
-    pygame.draw.circle(screen,GREEN,(490,170),20)
-    pygame.draw.circle(screen,GREEN,(730,250),20)
-    pygame.draw.circle(screen,GREEN,(650,250),20)
-    pygame.draw.circle(screen,GREEN,(570,250),20)
-    pygame.draw.circle(screen,GREEN,(490,250),20)
-    pygame.draw.circle(screen,GREEN,(730,330),20)
-    pygame.draw.circle(screen,GREEN,(650,330),20)
-    pygame.draw.circle(screen,GREEN,(570,330),20)
-    pygame.draw.circle(screen,GREEN,(490,330),20)
 
-    pygame.draw.line(screen,PURPLE,)
+    for circle in range(490,801,80):
+        pygame.draw.circle(screen,GREEN,(circle,80),20)
+        pygame.draw.circle(screen,GREEN,(circle,170),20)
+        pygame.draw.circle(screen,GREEN,(circle,260),20)
+        pygame.draw.circle(screen,GREEN,(circle,350),20)
 
+    for vertical in range(10,401,40):
+        pygame.draw.line(screen,PURPLE,(400,vertical),(10,vertical),width=2)
+
+    for horizon in range(10,401,40):
+        pygame.draw.line(screen,PURPLE,(horizon,375),(horizon,5),width=2)
+
+  
+    pygame.draw.line(screen,(255,0,4),(10,400),(450,140),width=40) 
 
     # --(leave below)--------------------------------------------------------
 
